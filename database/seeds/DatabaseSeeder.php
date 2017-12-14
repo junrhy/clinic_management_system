8a1ea2bede84986cafdee3f641481df2cd408d5f
Jun Rhy <jrcrodua@gmail.com>
2017-12-14 12:39:44 -0500

    initial files

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        // Role comes before User seeder here.
+        $this->call(RoleTableSeeder::class);
+        // User seeder will use the roles above created.
+        $this->call(UserTableSeeder::class);
+    }
+}
