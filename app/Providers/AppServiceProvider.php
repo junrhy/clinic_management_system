1f8fac224dc6a54d95638fc8ce7196fcabe5361e
Jun Rhy <jrcrodua@gmail.com>
2021-02-03 13:38:41 +0000

    add facades URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 use App\Model\Domain;
 
