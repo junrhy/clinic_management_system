08f5bc98818dbe6532f3ef17a3271133e10df4c2
Jun Rhy <jrcrodua@gmail.com>
2021-02-13 05:19:48 +0000

    change title system to software

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Clinic Management System</title>
+    <title>Clinic Management Software</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
