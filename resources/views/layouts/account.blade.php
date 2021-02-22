204623b74d3cd54e55a8a3eb0d4b0302e58ec2c4
Jun Rhy <jrcrodua@gmail.com>
2021-02-22 12:15:59 +0000

    add help menu

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -16,38 +16,45 @@
     <script src="https://kit.fontawesome.com/e3497de5a4.js" crossorigin="anonymous"></script>
 
     <style type="text/css">
-        #logo {
-            margin: 15px 0px;
-        }
-
         body {
             background-color: #01d8da;
+        }
+
+        .navbar-custom {
+          height: 90px;
+          vertical-align: middle;
+        }
+
+        .m-t-20 {
+            margin-top: 20px;
         }
     </style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-static-top navbar-custom">
             <div class="container">
-                <div class="">
-                    <div style="float:left;">
-                        <a class="logo" href="{{ url('/home') }}">
-                            @if (auth()->user()->type !='admin' && auth()->user()->client->logo != "")
-                                @if(env('FILESYSTEM_DRIVER') == 'spaces')
-                                <img src="{{ asset('https://file-server1.sfo2.digitaloceanspaces.com/' . auth()->user()->client->logo) }}" id="logo" class="" style="margin: 30px 3px 3px 3px;height:75px;width: 97%;">
-                                @endif
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="{{ url('/home') }}">
+                        @if (auth()->user()->type !='admin' && auth()->user()->client->logo != "")
 
-                                @if(env('FILESYSTEM_DRIVER') == 'public')
-                                <img src="{{ asset('storage/' . auth()->user()->client->logo) }}" id="logo" class="" style="margin: 30px 3px 3px 3px;height:60px;width: 97%;">
-                                @endif
-                            @else
-                                <img id="logo" src="/img/brand/bluewhalecms.png" style="height: 60px;">
+                            @if(env('FILESYSTEM_DRIVER') == 'spaces')
+                            <img src="{{ asset('https://file-server1.sfo2.digitaloceanspaces.com/' . auth()->user()->client->logo) }}" height="60">
                             @endif
-                        </a>
-                    </div>
-                    <div align="right">
-                        <a href="/home">Back</a> | 
-                        <a class="" href="{{ route('logout') }}"
+
+                            @if(env('FILESYSTEM_DRIVER') == 'public')
+                            <img src="{{ asset('storage/' . auth()->user()->client->logo) }}" height="60">
+                            @endif
+                        @else
+                            <img src="/img/brand/bluewhalecms.png" height="60">
+                        @endif
+                    </a>
+                </div>
+
+                <ul class="nav navbar-nav navbar-right m-t-20">
+                    <li class="nav-item"><a class="nav-link" href="/home">Dashboard</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
                           onclick="event.preventDefault();
                                    document.getElementById('logout-form').submit();">
                             Logout
@@ -56,8 +63,9 @@
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             {{ csrf_field() }}
                         </form>
-                    </div>
-                </div>
+                    </li>
+                </ul>
+
             </div>
         </nav>
         <br>
